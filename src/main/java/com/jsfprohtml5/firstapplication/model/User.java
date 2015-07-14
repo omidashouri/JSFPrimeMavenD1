7d08a75ab11013531b6e08165cab2062112d9ee7
Omid Ashouri <omidashouri@gmail.com>
2015-07-14 21:19:50 +0430

    update version JDK to 1.8 : 94/04/23

--- src/main/java/com/jsfprohtml5/firstapplication/model/User.java
+++ src/main/java/com/jsfprohtml5/firstapplication/model/User.java
@@ -1,8 +1,14 @@
 package com.jsfprohtml5.firstapplication.model;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ashouri on 7/7/2015.
@@ -12,6 +18,15 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String name;
     private String password;
+    private Map<String,String> headerList;
+
+
+    public void init(){
+        headerList = new HashMap<String, String>();
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        facesContext.getExternalContext().getRequestHeaderMap().forEach((k,v)->headerList.put(k.toString(),v.toString()));
+        headerList.forEach((k,v)->System.out.println("Key:"+k.toString()+",Value:"+v.toString()));
+    }
 
     public String getName() {
         return name;
